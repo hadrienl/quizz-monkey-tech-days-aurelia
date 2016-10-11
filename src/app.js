f5085e94bca3f26f51563f888ff1597057729d4c
Hadrien Lanneau <hadrien@lanneau.me>
2016-10-11 21:38:15 +0200

    Create a new quizz

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-export class App {}
+export class App {
+  configureRouter(config, router) {
+    config.title = 'Quizz Monkey!';
+    config.options.pushState = true;
+    config.map([{
+      route: '/',
+      name: 'home',
+      moduleId: 'home/home'
+    }]);
+    router.handleUnknownRoutes({
+      redirect: '/'
+    });
+    this.router = router;
+  }
+}
